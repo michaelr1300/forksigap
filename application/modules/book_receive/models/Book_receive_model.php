c25ea0bc73fca156887842047c5fbafb63b73391
esterinac <esterinachristina@gmail.com>
2021-03-13 11:17:28 +0700

    Migrate book stock and book receive tables
    -Migrate book stock and book receive tables
    - Get data from table book_receive

--- application/modules/book_receive/models/Book_receive_model.php
+++ application/modules/book_receive/models/Book_receive_model.php
@@ -2,4 +2,24 @@
 
 class Book_receive_model extends MY_Model{
 
+    public $per_page = 10;
+
+    public function filter_book_receive($filters, $page){
+        $book_receives = $this->select([])
+            ->when('keyword', $filters['keyword'])
+            ->get();
+    }
+
+    public function when($params, $data){
+        if ($data) {
+            if ($params == 'keyword') {
+                $this->group_start();
+                // $this->or_like('name', $data);
+                $this->or_like('book_title', $data);
+                $this->or_like('isbn', $data);
+                $this->group_end();
+            }
+        }
+        return $this;
+    }
 }
