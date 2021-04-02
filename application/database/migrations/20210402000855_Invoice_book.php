9a39974fa35124984a8fd9657897c907516231a5
Michael Roberto <michaelr1300@gmail.com>
2021-04-02 21:57:43 +0700

    Create 20210402000855_Invoice_book.php

---
+++ application/database/migrations/20210402000855_Invoice_book.php
@@ -0,0 +1,46 @@
+<?php
+
+class Migration_Invoice_book extends CI_Migration
+{
+
+    public function up()
+    {
+        $this->dbforge->add_field([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 10,
+                'auto_increment' => TRUE
+            ],
+            'invoice_id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'book_id' => [
+                'type' => 'VARCHAR',
+				'constraint' => 20,
+                'null' => TRUE,
+            ],
+            'qty' => [
+                'type' => 'INT',
+				'constraint' => 10,
+                'null' => TRUE,
+            ],
+            'discount' => [
+                'type' => 'INT',
+                'constraint' => 3,
+            ],
+            'price' => [
+                'type' => 'INT',
+                'constraint' => 10,
+                'null' => true
+            ]
+        ]);
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('invoice_book');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('invoice_book');
+    }
+}
