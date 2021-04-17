d23b5c2c992702ba41a651eb4d72519cc030b00d
esterinac <esterinachristina@gmail.com>
2021-04-17 15:48:41 +0700

    Fix table book_transfer, create table book_transfer_user & book_transfer_list

---
+++ application/database/migrations/20210416153648_Update_book_transfer.php
@@ -0,0 +1,35 @@
+<?php
+
+class Migration_update_book_transfer extends CI_Migration
+{
+    public function up()
+    {
+        //preparing_start_date
+        //preparing_deadline
+        //preparing_end_date
+
+        $this->dbforge->add_column('book_transfer', 
+        [
+            'preparing_deadline' => [
+                'type' => 'TIMESTAMP',
+                'null' => TRUE
+            ],           
+            'preparing_start_date' => [
+                'type' => 'TIMESTAMP',
+                'null' => TRUE
+            ],
+            'preparing_end_date' => [
+                'type' => 'TIMESTAMP',
+                'null' => TRUE
+            ],           
+            'finish_date' => [
+                'type' => 'TIMESTAMP',
+                'null' => TRUE
+            ],           
+        ]);
+    }
+
+    public function down(){
+        $this->dbforge->drop_column('invoice', 'preparing_staff');
+    }
+}
