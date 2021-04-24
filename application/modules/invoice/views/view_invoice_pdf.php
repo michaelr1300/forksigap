6134793b4ad3c1da9215b2464aa9c3a9f38371fe
Andrew Mulya <56527292+andrewmulya22@users.noreply.github.com>
2021-04-24 19:40:16 +0700

    tanggal

--- application/modules/invoice/views/view_invoice_pdf.php
+++ application/modules/invoice/views/view_invoice_pdf.php
@@ -1,10 +1,14 @@
 <!doctype html>
 <html>
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
     <title>Faktur UGM Press</title>
-    
+
     <style>
     body {
         font-size: 12px;
@@ -16,106 +20,116 @@
     .invoice-box {
         margin: auto;
         padding: 30px;
-       /** box-shadow: 0 0 10px rgba(0, 0, 0, .15); **/
+        /** box-shadow: 0 0 10px rgba(0, 0, 0, .15); **/
     }
-    
+
     .invoice-box table {
         width: 100%;
         line-height: inherit;
         text-align: left;
     }
-    
+
     .invoice-box table td {
         padding: 5px;
         vertical-align: top;
         font-size: 12px;
     }
- /**   .invoice-box table tr td:nth-child(2) {
+
+    /**   .invoice-box table tr td:nth-child(2) {
         text-align: left;
         font-size: 12px;
     } **/
-    
+
     .invoice-box table tr.top table td {
         padding-bottom: 20px;
     }
-    
+
     .invoice-box table tr.top table td.title {
         font-size: 45px;
         line-height: 45px;
         color: #333;
     }
-    
+
     .invoice-box table tr.information table td {
         padding-bottom: 5px;
     }
-    
-  .invoice-box table tr.heading td {
+
+    .invoice-box table tr.heading td {
         font-weight: bold;
     }
-    
+
     .invoice-box table tr.details td {
         padding-bottom: 20px;
     }
-    
-    .invoice-box table tr.item td{
+
+    .invoice-box table tr.item td {
         border-bottom: 1px solid #eee;
     }
-    
+
     .invoice-box table tr.item.last td {
         border-bottom: none;
     }
-        
-    .invoice-box table tr.main table thead{
+
+    .invoice-box table tr.main table thead {
         vertical-align: middle;
         text-align: center;
     }
-       
-     
-    .invoice-box table tr.main table{
+
+
+    .invoice-box table tr.main table {
         border: 1px solid black;
     }
-    
+
     .invoice-box table tr.total td:nth-child(7) {
         border-top: 2px solid #eee;
         font-weight: bold;
     }
-   
-        @media only screen and (max-width: 600px) {
+
+    @media only screen and (max-width: 600px) {
         .invoice-box table tr.top table td {
             width: 100%;
             display: block;
             text-align: center;
         }
-        
+
         .invoice-box table tr.information table td {
             width: 100%;
             display: block;
             text-align: center;
         }
-    }      
-        
-    table.invoice-table, thead.invoice-table, td.invoice-table, tr.invoice-table, .invoice-table{
+    }
+
+    table.invoice-table,
+    thead.invoice-table,
+    td.invoice-table,
+    tr.invoice-table,
+    .invoice-table {
         border: 1px solid black;
         border-collapse: collapse;
     }
+
     </style>
 </head>
+
 <body>
     <table style="width: 100%;">
         <tr>
             <td style="width:20%;">
-                <img src="<?= base_url('assets/images/logo_ugm_press.jpg'); ?>" style="width:100%; max-width:120px;">
+                <img
+                    src="<?= base_url('assets/images/logo_ugm_press.jpg'); ?>"
+                    style="width:100%; max-width:120px;"
+                >
             </td>
             <td style="width:80%">
                 <b>GADJAH MADA UNIVERSITY PRESS</b><br>
                 Jl. Sendok, Karanggayam CT VIII<br>
                 Caturtunggal Depok, Sleman, D.I. Yogyakarta 55281<br>
                 Telp/Fax (0274)-561037<br>
-                NPWP:01.246.578.7-542.000 - E-mail : ugmpress.ugm.ac.id | gmupress@ugm.ac.id 
+                NPWP:01.246.578.7-542.000 - E-mail : ugmpress.ugm.ac.id | gmupress@ugm.ac.id
             </td>
         </tr>
     </table>
-    
+
     <table style="width: 100%; margin-top: 4em;">
         <tr class="information">
             <td style="width:70%;">
@@ -126,23 +140,61 @@
                 No Faktur : <?= $invoice->number ?>
             </td>
             <td style="width:30%; vertical-align: top;">
-                Yogyakarta, <?= $invoice->issued_date ?><br>
-                Jatuh Tempo : <?= $invoice->due_date ?>
+                <?php $issued_date = strtotime($invoice->issued_date) ?>
+                <?php $due_date = strtotime($invoice->due) ?>
+                Yogyakarta, <?= date("Y-m-d", $issued_date) ?><br>
+                Jatuh Tempo : <?= date("Y-m-d", $due_date) ?>
             </td>
         </tr>
     </table>
 
-    <table class="invoice-table" style="width: 100%;">
-        <thead class="invoice-table" style="text-align: center;">
+    <table
+        class="invoice-table"
+        style="width: 100%;"
+    >
+        <thead
+            class="invoice-table"
+            style="text-align: center;"
+        >
             <tr class="invoice-table">
-                <th scope="col" width="5%" class="align-middle invoice-table" rowspan="2">No</th>
-                <th scope="col" width="5%" class="align-middle invoice-table" rowspan="2">Kode</th>
-                <th scope="col" width="54%" class="align-middle invoice-table" rowspan="2">Judul</th>
-                <th scope="col" width="5%" class="align-middle invoice-table" rowspan="2">Jml (eks)</th>
-                <th scope="col" width="5%" class="align-middle invoice-table" rowspan="2">Disc (%)</th>
-                <th scope="col" width="26%" class="align-middle invoice-table" colspan="2">Harga</th>
+                <th
+                    scope="col"
+                    width="5%"
+                    class="align-middle invoice-table"
+                    rowspan="2"
+                >No</th>
+                <th
+                    scope="col"
+                    width="5%"
+                    class="align-middle invoice-table"
+                    rowspan="2"
+                >Kode</th>
+                <th
+                    scope="col"
+                    width="54%"
+                    class="align-middle invoice-table"
+                    rowspan="2"
+                >Judul</th>
+                <th
+                    scope="col"
+                    width="5%"
+                    class="align-middle invoice-table"
+                    rowspan="2"
+                >Jml (eks)</th>
+                <th
+                    scope="col"
+                    width="5%"
+                    class="align-middle invoice-table"
+                    rowspan="2"
+                >Disc (%)</th>
+                <th
+                    scope="col"
+                    width="26%"
+                    class="align-middle invoice-table"
+                    colspan="2"
+                >Harga</th>
             </tr>
-            
+
             <tr>
                 <th class="align-middle invoice-table">Satuan</th>
                 <th class="align-middle invoice-table">Jumlah</th>
@@ -151,26 +203,38 @@
         <tbody>
             <?php $i = 1; ?>
             <?php foreach ($invoice_books as $invoice_book) : ?>
-            <tr class="invoice-table">
-                <td class="invoice-table"><?=$i++?></td>
-                <td class="invoice-table">A152</td>
-                <td class="invoice-table"><?= $invoice_book->book_title ?></td>
-                <td class="invoice-table" style="text-align: right;"><?= $invoice_book->qty ?></td>
-                <td class="invoice-table" style="text-align: right;"><?= $invoice_book->discount ?></td>
-                <td class="invoice-table" style="text-align: right;"><?= $invoice_book->price ?></td>
-                <td class="invoice-table" style="text-align: right;"><?= $invoice_book->price * $invoice_book->qty * (1 - $invoice_book->discount/100) ?></td>
-            </tr>
-            <?php endforeach?>
+                <tr class="invoice-table">
+                    <td class="invoice-table"><?= $i++ ?></td>
+                    <td class="invoice-table">A152</td>
+                    <td class="invoice-table"><?= $invoice_book->book_title ?></td>
+                    <td
+                        class="invoice-table"
+                        style="text-align: right;"
+                    ><?= $invoice_book->qty ?></td>
+                    <td
+                        class="invoice-table"
+                        style="text-align: right;"
+                    ><?= $invoice_book->discount ?></td>
+                    <td
+                        class="invoice-table"
+                        style="text-align: right;"
+                    ><?= $invoice_book->price ?></td>
+                    <td
+                        class="invoice-table"
+                        style="text-align: right;"
+                    ><?= $invoice_book->price * $invoice_book->qty * (1 - $invoice_book->discount / 100) ?></td>
+                </tr>
+            <?php endforeach ?>
 
-            <?php 
-                $total=0;
-                foreach($invoice_books as $invoice_book){
-                    $total += $invoice_book->price;
-                }
+            <?php
+            $total = 0;
+            foreach ($invoice_books as $invoice_book) {
+                $total += $invoice_book->price;
+            }
             ?>
         </tbody>
     </table>
-    
+
     <table style="width: 100%;">
         <tr>
             <td style="width:50%"></td>
@@ -181,53 +245,93 @@
         <tr>
             <td scope="col"></td>
             <td scope="col">Diskon</td>
-            <td scope="col" style="text-align: right;">0%</td>
-            <td scope="col" style="text-align: right; border-bottom: 1px solid black">0</td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            >0%</td>
+            <td
+                scope="col"
+                style="text-align: right; border-bottom: 1px solid black"
+            >0</td>
         </tr>
         <tr>
             <td scope="col"></td>
             <td scope="col"></td>
-            <td scope="col" style="text-align: right;"></td>
-            <td scope="col" style="text-align: right;"><?php $total?></td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            ></td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            ><?php $total ?></td>
         </tr>
         <tr>
             <td scope="col"></td>
             <td scope="col">Ongkos Kirim</td>
-            <td scope="col" style="text-align: right;">0</td>
-            <td scope="col" style="text-align: right; border-bottom: 1px solid black">Total + Ongkir</td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            >0</td>
+            <td
+                scope="col"
+                style="text-align: right; border-bottom: 1px solid black"
+            >Total + Ongkir</td>
         </tr>
         <tr>
             <td scope="col"></td>
             <td scope="col"><b>Jumlah</b></td>
-            <td scope="col" style="text-align: right;"></td>
-            <td scope="col" style="text-align: right; border-bottom: 4px double black"><b><?php $total?></b></td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            ></td>
+            <td
+                scope="col"
+                style="text-align: right; border-bottom: 4px double black"
+            ><b><?php $total ?></b></td>
         </tr>
         <tr>
             <td scope="col"></td>
             <td scope="col"></td>
-            <td scope="col" colspan="2" style="text-align: right;"><b>Total dalam huruf</b></td>
+            <td
+                scope="col"
+                colspan="2"
+                style="text-align: right;"
+            ><b>Total dalam huruf</b></td>
         </tr>
         <tr>
             <td scope="col"></td>
             <td scope="col">Bayar</td>
-            <td scope="col" style="text-align: right;"></td>
-            <td scope="col" style="text-align: right;"><?php $total?></td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            ></td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            ><?php $total ?></td>
         </tr>
         <tr>
             <td scope="col"></td>
             <td scope="col">Kurang</td>
-            <td scope="col" style="text-align: right;"></td>
-            <td scope="col" style="text-align: right;">0</td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            ></td>
+            <td
+                scope="col"
+                style="text-align: right;"
+            >0</td>
         </tr>
     </table>
-    
+
     <table style="width: 100%;">
         <tr>
             <td style="width:50%"></td>
             <td style="width:20%"></td>
         </tr>
         <tr class="information">
-            <td >
+            <td>
                 <b>Keterangan :</b><br>
                 Bukti pembayaran mohon dikirimkan melalui email: ugmpress@ugm.ac.id<br>
                 atau whatsapp: 081228478888<br>
@@ -237,7 +341,7 @@
                 2. BNI Cab. UGM Yogyakarta : 0039226571<br>
                 <a style="padding-left: 14px;">a/n GAMA PRESS</a>
             </td>
-            
+
             <td style="text-align: center;">
                 a.n. Ka. Pemasaran<br><br><br><br><br><br><br>
                 <i>Adm. Pemasaran</i>
